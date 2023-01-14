2265901c830f62e8e809f8ab81d48fc9e9fff67e
michitaka <iida.michitaka@isid.co.jp>
2023-01-14 20:32:46 +0900

    ｓ

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function onSubmit(){
     var text = document.getElementById('answer').value;
     text=  text +"\n"+ document.getElementById('no').value;
     
-    const msg = "【送信】\n" + text;
+    const msg = "【送信完了】\n" + text;
     console.log(msg);
     sendText(msg);
 
